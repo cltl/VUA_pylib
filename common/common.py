64782e4eda7dfb8db04fd237129704275895b51b
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2013-11-21 13:06:53 +0100

    updated normalize_pos

--- common/common.py
+++ common/common.py
@@ -14,15 +14,15 @@ def get_max_distr_dict(my_dict):
 def normalize_pos(pos):
     pos = pos.lower()
     new_pos = pos
-    if pos in ['adj']:
+    if pos in ['adj','a'] or pos[0:2]=='jj':
         new_pos = 'a'
-    elif pos in ['adverb']:
+    elif pos in ['adverb','r','']: or pos[0:2]=='rb'
         new_pos = 'r'
     elif pos in ['anypos']:
         new_pos = '*'
-    elif pos in ['noun']:
+    elif pos in ['noun','n'] or pos[0:2]=='nn' or pos[0:2]=='np'
         new_pos = 'n'
-    elif pos in ['verb']:
-        new_pos = 'p'
+    elif pos in ['verb','v'] or pos[0]=='v':
+        new_pos = 'v'
     return new_pos
 
